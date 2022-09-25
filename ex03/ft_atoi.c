80e41655618f9883232cfbfd11ba644baf8628cc
Theo Potillion <tpotilli@e2r7p22.clusters.42paris.fr>
2022-09-25 15:24:36 +0200

    42

---
+++ ex03/ft_atoi.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tpotilli <tpotilli@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/23 10:09:06 by tpotilli          #+#    #+#             */
+/*   Updated: 2022/09/25 13:11:50 by tpotilli         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_atoi(char *str)
+{
+	int	i;
+	int	resultat;
+	int	sign;
+	int	cpt;
+
+	resultat = 0;
+	i = 0;
+	cpt = 0;
+	sign = 1;
+	while (str[i] == ' ' || str[i] >= '9' || str[i] <= 13)
+		i++;
+	while (str[i] == '+' || str[i] == '-')
+	{
+		if (str[i] == '-')
+			cpt++;
+		i++;
+	}
+	if (cpt % 2 != 0)
+		sign = -1;
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		resultat = resultat * 10 + str[i] - '0';
+		i++;
+	}
+	return (resultat * sign);
+}
